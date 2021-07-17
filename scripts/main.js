3bacca6f6ff2c6553144161a5828e41c4f9e24d4
Juan Manuel Gallego <juan@Juans-MacBook-Pro.local>
2021-07-17 13:44:20 -0300

    update del repositorio

--- scripts/main.js
+++ scripts/main.js
@@ -10,18 +10,14 @@ miImage.onclick = function () {
 let miBoton = document.querySelector('button');
 let miTitulo = document.querySelector( 'h1');
 function estableceNombreUsuario() {
-    let miNombre = prompt('Por favor, ingresa tu nombre.');
-    localStorage.setItem('nombre', miNombre);
-    miTitulo.textContent = 'Mozilla es genial,' + miNombre;
-}
-if (!localStorage.getItem('nombre')) {
-    estableceNombreUsuario();
-}
-else {
-    let nombreAlmacenado = localStorage.getItem('nombre');
-    miTitulo.textContent = 'Mozilla es genial,' + nombreAlmacenado;
-}
+    let miNombre = prompt('Introduzca su nombre.');
+    if(!miNombre) {
+      estableceNombreUsuario();
+    } else {
+      localStorage.setItem('nombre', miNombre);
+      miTitulo.innerHTML = 'Mozilla is genial, ' + miNombre;
+    }
+  }
 miBoton.onclick = function() {
     estableceNombreUsuario() ;
 }
-
